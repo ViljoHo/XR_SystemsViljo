a3754b05393d99d1de8d3031751e32650b2d6ff0
ViljoH <78697830+ViljoHo@users.noreply.github.com>
2025-02-25 13:20:04 +0200

    Added colliders to hands

--- HW3_HandsAndGame/Assets/HandController.cs
+++ HW3_HandsAndGame/Assets/HandController.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[ExecuteInEditMode] // Suorittaa koodin myös editorissa ilman pelitilaa
 public class HandController : MonoBehaviour
 {
     public InputActionReference gripInput;
     public InputActionReference triggerInput;
-    
+
     private Animator animator;
     private bool wasPressed = false;
 
@@ -13,8 +14,11 @@ public class HandController : MonoBehaviour
     {
         animator = GetComponent<Animator>();
 
-        gripInput.action.Enable();
-        triggerInput.action.Enable();
+        if (Application.isPlaying) // Estetään turhat kutsut editoritilassa
+        {
+            gripInput.action.Enable();
+            triggerInput.action.Enable();
+        }
     }
 
     void Update()
@@ -24,22 +28,75 @@ public class HandController : MonoBehaviour
             return;
         }
 
-        bool isPressed = gripInput.action.IsPressed();
-
-        if (isPressed && !wasPressed)
+        if (Application.isPlaying) // PELITILASSA: päivitä inputin mukaan
         {
-            Debug.Log("Nappia painetaan!");
+            bool isPressed = gripInput.action.IsPressed();
+
+            if (isPressed && !wasPressed)
+            {
+                Debug.Log("Nappia painetaan!");
+            }
+
+            wasPressed = isPressed;
+
+            float grip = gripInput.action.ReadValue<float>();
+            float trigger = triggerInput.action.ReadValue<float>();
+
+            animator.SetFloat("Grip", grip);
+            animator.SetFloat("Trigger", trigger);
         }
-        
-        wasPressed = isPressed;
-
-        float grip = gripInput.action.ReadValue<float>();
-        float trigger = triggerInput.action.ReadValue<float>();
-
-
-        //Debug.Log("gribb" + grip);
-        //Debug.Log("trigger" + trigger);
-        animator.SetFloat("Grip", grip);
-        animator.SetFloat("Trigger", trigger);
+        else // EDITORITILASSA: Pakota animaatio päälle
+        {
+            animator.Update(0);
+        }
     }
 }
+
+
+
+
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+
+// public class HandController : MonoBehaviour
+// {
+//     public InputActionReference gripInput;
+//     public InputActionReference triggerInput;
+    
+//     private Animator animator;
+//     private bool wasPressed = false;
+
+//     private void Start()
+//     {
+//         animator = GetComponent<Animator>();
+
+//         gripInput.action.Enable();
+//         triggerInput.action.Enable();
+//     }
+
+//     void Update()
+//     {
+//         if (animator == null)
+//         {
+//             return;
+//         }
+
+//         bool isPressed = gripInput.action.IsPressed();
+
+//         if (isPressed && !wasPressed)
+//         {
+//             Debug.Log("Nappia painetaan!");
+//         }
+        
+//         wasPressed = isPressed;
+
+//         float grip = gripInput.action.ReadValue<float>();
+//         float trigger = triggerInput.action.ReadValue<float>();
+
+
+//         //Debug.Log("gribb" + grip);
+//         //Debug.Log("trigger" + trigger);
+//         animator.SetFloat("Grip", grip);
+//         animator.SetFloat("Trigger", trigger);
+//     }
+// }
